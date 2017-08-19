20f419ea4bd1f9416318c6b47a373cccf478a52e
Kento Lauzon <kento.otomo-lauzon.1@ulaval.ca>
2017-08-19 14:25:43 -0400

    Added daily proxy command

---
+++ src/main/java/com/lambdanum/raids/DailyCommand.java
@@ -0,0 +1,60 @@
+package com.lambdanum.raids;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+
+public class DailyCommand implements ICommand {
+
+    private LootCommand lootCommand = new LootCommand();
+
+    @Override
+    public String getName() {
+        return "daily";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "daily";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if (sender instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)sender;
+            lootCommand.execute(server,sender, new String[]{player.getName(),"daily"});
+        }
+    }
+
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand iCommand) {
+        return 0;
+    }
+}
