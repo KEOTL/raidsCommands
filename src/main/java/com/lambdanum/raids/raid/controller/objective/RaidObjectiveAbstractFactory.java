d94a2d8c8d032503df3c57ea0fee258070d93bc2
KEOTL <kento.otomo-lauzon.1@ulaval.ca>
2018-01-01 11:23:20 -0500

    timer objective + random-timer objective

--- src/main/java/com/lambdanum/raids/raid/controller/objective/RaidObjectiveAbstractFactory.java
+++ src/main/java/com/lambdanum/raids/raid/controller/objective/RaidObjectiveAbstractFactory.java
@@ -2,8 +2,10 @@ package com.lambdanum.raids.raid.controller.objective;
 
 import com.lambdanum.raids.model.Position;
 
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class RaidObjectiveAbstractFactory {
 
@@ -15,7 +17,22 @@ public class RaidObjectiveAbstractFactory {
                 return new AskItemAtPositionObjectivePoller(subscriber, args.get(1), new Position(Integer.parseInt(args.get(2)), Integer.parseInt(args.get(3)), Integer.parseInt(args.get(4))));
             case "bring-player":
                 return new BringPlayerObjectivePoller(subscriber, new Position(Integer.parseInt(args.get(1)), Integer.parseInt(args.get(2)), Integer.parseInt(args.get(3))));
+            case "timer":
+                String delay = args.get(1);
+                ZonedDateTime stopTime = ZonedDateTime.now().plusSeconds(Integer.parseInt(delay));
+                return new TimerObjectivePoller(subscriber, stopTime);
+            case "random-timer":
+                String lowerDelayBound = args.get(1);
+                String higherDelayBound = args.get(2);
+                int waitTime = getRandomIntegerBetween(Integer.parseInt(lowerDelayBound), Integer.parseInt(higherDelayBound));
+                ZonedDateTime randomStopTime = ZonedDateTime.now().plusSeconds(waitTime);
+                return new TimerObjectivePoller(subscriber, randomStopTime);
         }
         throw new UnknownObjectiveTypeException(objectiveType);
+    }
+
+    private int getRandomIntegerBetween(int min, int max) {
+        Random random = new Random();
+        return min + random.nextInt(max - min);
     }
 }
